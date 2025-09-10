c683635999de0a0d33bb71e0c83f63341b7c156c
shalaginoid <shalaginoid@mail.ru>
2025-09-10 08:43:07 +0500

    commit

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const config = {
   dbConnectionString:
     'Data Source=s-eens-mss-sql4;Database=EensGosuslugiNotifications;User Id=gosuslugi;Password=123!@#qwe;TrustServerCertificate=true',
-  // authUrl: 'http://localhost:3001/api/gosuslugi/auth',
 };
 
 export default config;
